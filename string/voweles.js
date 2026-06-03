9ced6140bc0d801bf847ca8f30a433826d17462a
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-06-03 10:51:15 +0530

    Implement vowel counting in voweles.js
    Added vowel counting logic using a loop and a regular expression.

--- string/voweles.js
+++ string/voweles.js
@@ -12,6 +12,24 @@ for(let i=0; i < name.length;i++){
     }
 }
 console.log(count)
+// using the or || operator and not using the include() function are method
+let name = "Ramakrishna katravath"
+let count = 0
+for(let i = 0; i < name.length;i++){
+    if( name[i] === 'a' ||
+        name[i] === 'e' ||
+        name[i] === 'i' ||
+        name[i] === 'o' ||
+        name[i] === 'u' ||
+        name[i] === 'A' ||
+        name[i] === 'E' ||
+        name[i] === 'I' ||
+        name[i] === 'O' ||
+        name[i] === 'U'
+       ){
+        count ++
+}}
+console.log(count)
 
 // Using Regular Expression and ternary operator
 let name = "hello";
