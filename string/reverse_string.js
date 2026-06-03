91c9a56b57bd6bb674b5bdde07394b49da602809
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-06-03 09:41:06 +0530

    Update reverse_string.js

--- string/reverse_string.js
+++ string/reverse_string.js
@@ -1,61 +1,61 @@
 
-// //  reverse a string using the splite method
-// let text = "Ramakrishna";
+//  reverse a string using the splite method
+let text = "Ramakrishna";
 
-// let reversed = text.split("").reverse().join("")
+let reversed = text.split("").reverse().join("")
 
-// console.log(reversed)
-
-
-// //  reverse a string using the loop
-
-// let name = "Kirthana";
-
-// let reversed = "";
-
-// for(let i = name.length-1; i >=0; i--){
-//     reversed += name[i]
-// }
-// console.log(reversed)
-
-// // reverse a string using for but not using reverse word
-
-// let name1 = "prasana";
-// let result =  "";
-// for(let char of name1){
-//     result = char + result
-
-// }
-// console.log(result)
-
-// // uesing the recusion fuction
-
-// function reverseString(str) {
-//     if (str === "") {
-//         return "";
-//     }
-
-//     return reverseString(str.substr(1)) + str[0];
-// }
-
-// console.log(reverseString("hello"));
+console.log(reversed)
 
 
-// // Using Stack Logic
+//  reverse a string using the loop
 
-// let text = "hello";
-// let stack = [];
+let name = "Kirthana";
 
-// // Push characters into stack
-// for (let char of text) {
-//     stack.push(char);
-// }
+let reversed = "";
 
-// let reversed = "";
+for(let i = name.length-1; i >=0; i--){
+    reversed += name[i]
+}
+console.log(reversed)
 
-// // Pop characters from stack
-// while (stack.length > 0) {
-//     reversed += stack.pop();
-// }
+// reverse a string using for but not using reverse word
 
-// console.log(reversed);
+let name1 = "prasana";
+let result =  "";
+for(let char of name1){
+    result = char + result
+
+}
+console.log(result)
+
+// uesing the recusion fuction
+
+function reverseString(str) {
+    if (str === "") {
+        return "";
+    }
+
+    return reverseString(str.substr(1)) + str[0];
+}
+
+console.log(reverseString("hello"));
+
+
+// Using Stack Logic
+
+let text = "hello";
+let stack = [];
+
+// Push characters into stack
+for (let char of text) {
+    stack.push(char);
+}
+
+let reversed = "";
+
+// Pop characters from stack
+while (stack.length > 0) {
+    reversed += stack.pop();
+}
+
+console.log(reversed);
